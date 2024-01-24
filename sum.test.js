11cf61e8b4388bb62f64fcdd0e27944c49d1788a
nadiasokolan <nadia.sokolan@vitechteam.com>
2024-01-24 14:23:12 +0200

    Add subtract test

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,10 @@
 const sum = require('./sum');
+const sub = require("./sub");
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(3, 2)).toBe(3);
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('sub 5 - 3 to equal 2', () => {
+    expect(sub(5, 3)).toBe(2);
 });
